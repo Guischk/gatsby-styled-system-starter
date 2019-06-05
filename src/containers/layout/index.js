d49a6db79d83307fb455a420d0057e1f8c47228a
Guiz <6798336+Guischk@users.noreply.github.com>
2019-06-05 08:46:07 +0200

    Initial commit

---
+++ src/containers/layout/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { ThemeProvider } from 'styled-components';
+import theme from '../../theme';
+
+const Layout = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Layout;
---
+++ src/theme/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  primary: 'rebecapurple',
+  black: 'black',
+  white: 'whtie',
+};
+
+export default colors;
